6ead55714af4c7e8af9894566601ecea96e4dbb6
agarzonp <agarzonp@gmail.com>
2017-03-19 15:41:23 +0100

    Print properlu number of created players debug message

--- Source/ColouringBook/ColouringBookGameMode.cpp
+++ Source/ColouringBook/ColouringBookGameMode.cpp
@@ -10,3 +10,40 @@ AColouringBookGameMode::AColouringBookGameMode()
 	DefaultPawnClass = AColouringBookPawn::StaticClass();
 }
 
+void AColouringBookGameMode::StartPlay()
+{
+	int numPlayers = 4; // FIXME: This number should problably come from UI, unless we are using a players pop in/out feature
+	CreatePlayers(numPlayers);
+
+	AGameModeBase::StartPlay();
+}
+
+void AColouringBookGameMode::CreatePlayers(int numPlayers)
+{
+	// TO-DO: Investigate how to surpass MaxSplitscreenPlayers constraint because it is the one that doesn´t allow to create 
+	// more than 4 players (regardless of using split screen or not)
+	const int MAX_PLAYERS = GetWorld()->GetGameInstance()->GetGameViewportClient()->MaxSplitscreenPlayers;
+
+	// somehow/somewhere the first player is always created...
+	// TO-DO: Investigate if we are able to control first player creation at this point
+	int32 playerIndex = GetWorld()->GetGameInstance()->GetNumLocalPlayers();
+
+	int numPlayersCreated = playerIndex;
+
+	while (playerIndex < numPlayers && playerIndex < MAX_PLAYERS)
+	{
+		if (UGameplayStatics::CreatePlayer(GetWorld()))
+		{
+			numPlayersCreated++;
+		}
+
+		playerIndex++;
+	}
+
+	// print debug message
+	FString debugMessage;
+	debugMessage.AppendInt(numPlayersCreated);
+	debugMessage.Append(" Players created");
+	GEngine->AddOnScreenDebugMessage(INDEX_NONE, 5.0f, FColor::Green, debugMessage);
+}
+
